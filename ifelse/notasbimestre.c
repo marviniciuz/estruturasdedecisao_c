66a8f039471e874f6cdd6a6cc8d5c03656b99784
Marcus Vinicius <eumarcusviniciusp@gmail.com>
2022-10-09 21:24:30 -0300

    add exercicios de notas bimestrais

---
+++ ifelse/notasbimestre.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+float main()
+{
+
+    float nota1,nota2,nota3,nota4;
+    float media;
+
+    printf("digite a nota do primeiro bimestre");
+    scanf("%f",&nota1);
+    
+    printf("digite a nota do primeiro bimestre");
+    scanf("%f",&nota1);
+
+    printf("digite a nota do primeiro bimestre");
+    scanf("%f",&nota1);
+
+    printf("digite a nota do primeiro bimestre");
+    scanf("%f",&nota1);
+
+    media = (nota1,nota2,nota3,nota4)/4;
+
+    if(media >= 7){
+        printf("aluno aprovado");
+    }
+    else{
+        printf("aluno reprovado");
+    }
+
+    return 0;
+}
