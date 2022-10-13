af2d907160288dd9491c0a33d1d7744fcd43ec3c
Marcus Vinicius <eumarcusviniciusp@gmail.com>
2022-10-13 11:27:48 -0300

    adding exemplo10.c

---
+++ logical_operators/exemplo10.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+
+    int num1,num2;
+    scanf("%i%i",&num1,&num2);
+
+     if (num1 >= 10 && num2 >= 10){
+        printf("Numero invalido\n");
+    }  
+    else{
+        printf("Numero valido\n");
+    }     
+}   
