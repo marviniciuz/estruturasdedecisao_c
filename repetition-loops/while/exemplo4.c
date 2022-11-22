c2b90cc7bfdba948a6e62c2d8bf22acf03114bb1
Marcus Vinicius <eumarcusviniciusp@gmail.com>
2022-11-21 23:08:11 -0300

    novos exemplos simples de while

---
+++ repetition-loops/while/exemplo4.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+  int contador = 1; 
+
+  while (contador <= 10) 
+  {
+    printf("%d\n", contador); 
+    contador = (contador + 1);
+
+  }  
+  
+  return 0;
+}
---
+++ repetition-loops/while/exemplo8.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int contador = 0; 
+  int pare = 10;
+
+  while (contador != pare) 
+  {
+    printf("%d\n", contador); 
+    
+    contador++;
+  }  
+  
+  return 0;
+}
