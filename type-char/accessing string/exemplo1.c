35c89a7ec26f59c694147a82e895e7eb4f12d33d
Marcus Vinicius <eumarcusviniciusp@gmail.com>
2022-12-17 23:53:36 -0300

    exmplos de como acessar valores do tipo char

---
+++ type-char/accessing string/exemplo1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char str[] = "meu string";
+	char letra;
+
+	printf("%c\n",str[2]); /*imprime o 3o caracter*/
+
+	letra = str[8];
+
+	printf("%c\n",letra); /*imprime o 9o caracter*/
+
+}
